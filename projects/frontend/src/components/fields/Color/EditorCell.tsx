7ff492c704a8b84f3532839f668877d7564c51ce
Jumpei Ogawa <git@phanective.org>
2024-10-06 13:49:37 +0900

    refactor: upgrade `react-color-palette`

--- projects/frontend/src/components/fields/Color/EditorCell.tsx
+++ projects/frontend/src/components/fields/Color/EditorCell.tsx
@@ -1,6 +1,6 @@
 import { IEditorCellProps } from "@src/components/fields/types";
-import { ColorPicker, toColor } from "react-color-palette";
-import "react-color-palette/lib/css/styles.css";
+import { ColorPicker, ColorService } from "react-color-palette";
+import "react-color-palette/css";
 
 import { Box } from "@mui/material";
 
@@ -8,11 +8,10 @@ export default function Color({ value, onChange }: IEditorCellProps) {
   return (
     <Box sx={{ "& .rcp": { border: 0 } }}>
       <ColorPicker
-        width={240}
         height={180}
-        color={value?.hex ? toColor("hex", value.hex) : toColor("hex", "#fff")}
+        color={ ColorService.convert("hex", value?.hex ?? "#fff") }
         onChange={onChange}
-        alpha
+        hideAlpha={ false }
       />
     </Box>
   );
