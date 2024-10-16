de72d680a5161ee899e9db4f1523d6531ce78535
Jumpei Ogawa <git@phanective.org>
2024-10-16 11:25:34 +0900

    chore: remove unused Vite plugins
    - Currently, this product cannot be compiled with `@vitejs/plugin-react-swc`.
    - You can import SVGs directly into React components without `vite-plugin-svgr`.

--- projects/frontend/vite.config.ts
+++ projects/frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,8 +31,6 @@ export default defineConfig({
     }),
     // To enable import '@src/' type of imports
     viteTsconfigPaths(),
-    // To enable import of SVG as React component
-    svgrPlugin(),
   ],
   test: {
     globals: true,
