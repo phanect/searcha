f5eba5e36c2bf2f2a1b5078bde3972fd71189210
Jumpei Ogawa <git@phanective.org>
2024-11-05 08:58:47 +0900

    chore: remove `unbundled` ESLint rule
    The product would be deployed as a bundled app

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,10 @@
-import { core, nodejs, unbundled } from "@phanect/lint";
+import { core, nodejs } from "@phanect/lint";
 import { react } from "@phanect/lint-react";
 
 /** @type { import("eslint").Linter.Config[] } */
 export default [
   ...core,
   ...nodejs,
-  ...unbundled,
   ...react,
 
   {
