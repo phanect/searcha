a934850ff7c9eb8729538882d6ce9de2e7e38cea
Jumpei Ogawa <git@phanective.org>
2024-11-06 07:40:19 +0900

    refactor: remove unused function `httpsPost()`

--- workspaces/backend/src/utils.ts
+++ workspaces/backend/src/utils.ts
@@ -1,36 +1,4 @@
-import https from "https";
 import { asyncExecute } from "./terminalUtils";
-
-export function httpsPost({ body, ...options }: any) {
-  return new Promise((resolve, reject) => {
-    const req = https.request(
-      {
-        method: "POST",
-        ...options,
-      },
-      (res) => {
-        res.setEncoding("utf8");
-        let body = "";
-        res.on("data", (chunk) => {
-          body += chunk;
-        });
-        res.on("end", () => {
-          try {
-            const respData = JSON.parse(body);
-            resolve(respData);
-          } catch (error) {
-            console.log({ body, options });
-          }
-        });
-      }
-    );
-    req.on("error", reject);
-    if (body) {
-      req.write(JSON.stringify(body));
-    }
-    req.end();
-  });
-}
 
 const getRequires = (code: string) =>
   code.match(/(?<=((= |=|\n* )require\(("|')))[^.].*?(?=("|')\))/g);
