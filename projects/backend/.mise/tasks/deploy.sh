df2de475ae4172dfd01717606dfeca017986a2a0
Jumpei Ogawa <git@phanective.org>
2026-03-11 09:36:43 +0900

    Revert "chore(prj): migrate to pnpm"
    This reverts commit db36b5fe691b37fe127cc610c8fb3967c6f511f4.

--- projects/backend/.mise/tasks/deploy.sh
+++ projects/backend/.mise/tasks/deploy.sh
@@ -26,7 +26,7 @@ then
    helpFunction
 fi
 
-pnpm run build
+npm run build
 gcloud config set project $project_id
 gcloud builds submit --tag gcr.io/$project_id/$name
 gcloud run deploy $name --image gcr.io/$project_id/$name --platform managed --cpu 1000m --memory 2Gi --allow-unauthenticated --region $region
