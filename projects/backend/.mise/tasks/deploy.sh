af4066bb6de9cd9b2bec796d6ba109603198e927
Jumpei Ogawa <git@phanective.org>
2026-03-11 09:17:40 +0900

    ci: set Google Cloud project ID via environment variable, not via option

--- projects/backend/.mise/tasks/deploy.sh
+++ projects/backend/.mise/tasks/deploy.sh
@@ -4,32 +4,14 @@ set -eu
 
 name=rowy-backend
 region=us-central1
-helpFunction()
-{
-   echo "Usage: ./deploy.sh --project [YOUR GCLOUD PROJECT ID]"
-   exit 0
-}
 
-while test $# -gt 0; do
-           case "$1" in
-                --project)
-                    shift
-                    project_id=$1
-                    shift
-                    ;;
-                *)
-                   echo "$1 is not a recognized flag!"
-                   return 1;
-                   ;;
-          esac
-  done
-
-if [[ -z "$project_id" ]];
+if [[ -z "$GCP_PROJECT_ID" ]];
 then
-   helpFunction
+  echo "Environment variable GCP_PROJECT_ID is not set."
+  exit 1
 fi
 
 pnpm run build
-gcloud config set project $project_id
-gcloud builds submit --tag gcr.io/$project_id/$name
-gcloud run deploy $name --image gcr.io/$project_id/$name --platform managed --cpu 1000m --memory 2Gi --allow-unauthenticated --region $region
+gcloud config set project $GCP_PROJECT_ID
+gcloud builds submit --tag gcr.io/$GCP_PROJECT_ID/$name
+gcloud run deploy $name --image gcr.io/$GCP_PROJECT_ID/$name --platform managed --cpu 1000m --memory 2Gi --allow-unauthenticated --region $region
